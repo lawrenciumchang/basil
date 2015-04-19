809800bc1dc0892b0002de527017e906e18f3f65
anniediep <annie_diep@utexas.edu>
2015-04-19 14:44:06 -0500

    Category Name in Category View

--- app/src/main/java/com/lawrencium/basil/Act_CategoryView.java
+++ app/src/main/java/com/lawrencium/basil/Act_CategoryView.java
@@ -1,10 +1,12 @@
 package com.lawrencium.basil;
 
+import android.app.ActionBar;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import static com.lawrencium.basil.R.id.txt_week1;
@@ -21,6 +23,15 @@ public class Act_CategoryView extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_category_view);
+        ActionBar actionBar = getActionBar();
+        actionBar.hide();
+
+        Bundle bundle = getIntent().getExtras();
+        String catName = bundle.getString("CAT_NAME");
+        String catTotal = bundle.getString("CAT_TOTAL");
+
+        TextView monthlyExpense = (TextView) findViewById(R.id.monthlyExpense);
+        monthlyExpense.setText(catName +" Monthly Expense");
 
         txt_week1 = (TextView) findViewById(R.id.txt_week1);
         // hide until its title is clicked
@@ -66,47 +77,71 @@ public class Act_CategoryView extends Activity {
 
         switch(v.getId()){
             case R.id.week1:
-                if(txt_week1.isShown()){
-                    Fx.slide_up(this, txt_week1);
-                    txt_week1.setVisibility(View.GONE);
-                }
-                else{
-                    txt_week1.setVisibility(View.VISIBLE);
-                    Fx.slide_down(this, txt_week1);
-                }
+                txt_week1.setVisibility( txt_week1.isShown()
+                        ? View.GONE
+                        : View.VISIBLE );
                 break;
             case R.id.week2:
-                if(txt_week2.isShown()){
-                    Fx.slide_up(this, txt_week2);
-                    txt_week2.setVisibility(View.GONE);
-                }
-                else{
-                    txt_week2.setVisibility(View.VISIBLE);
-                    Fx.slide_down(this, txt_week2);
-                }
+                txt_week2.setVisibility( txt_week2.isShown()
+                        ? View.GONE
+                        : View.VISIBLE );
                 break;
             case R.id.week3:
-                if(txt_week3.isShown()){
-                    Fx.slide_up(this, txt_week3);
-                    txt_week3.setVisibility(View.GONE);
-                }
-                else{
-                    txt_week3.setVisibility(View.VISIBLE);
-                    Fx.slide_down(this, txt_week3);
-                }
+                txt_week3.setVisibility( txt_week3.isShown()
+                        ? View.GONE
+                        : View.VISIBLE );
                 break;
             case R.id.week4:
-                if(txt_week4.isShown()){
-                    Fx.slide_up(this, txt_week4);
-                    txt_week4.setVisibility(View.GONE);
-                }
-                else{
-                    txt_week4.setVisibility(View.VISIBLE);
-                    Fx.slide_down(this, txt_week4);
-                }
+                txt_week4.setVisibility( txt_week4.isShown()
+                        ? View.GONE
+                        : View.VISIBLE );
 
 
         }
+
+//        switch(v.getId()){
+//            case R.id.week1:
+//                if(txt_week1.isShown()){
+//                    Fx.slide_up(this, txt_week1);
+//                    txt_week1.setVisibility(View.GONE);
+//                }
+//                else{
+//                    txt_week1.setVisibility(View.VISIBLE);
+//                    Fx.slide_down(this, txt_week1);
+//                }
+//                break;
+//            case R.id.week2:
+//                if(txt_week2.isShown()){
+//                    Fx.slide_up(this, txt_week2);
+//                    txt_week2.setVisibility(View.GONE);
+//                }
+//                else{
+//                    txt_week2.setVisibility(View.VISIBLE);
+//                    Fx.slide_down(this, txt_week2);
+//                }
+//                break;
+//            case R.id.week3:
+//                if(txt_week3.isShown()){
+//                    Fx.slide_up(this, txt_week3);
+//                    txt_week3.setVisibility(View.GONE);
+//                }
+//                else{
+//                    txt_week3.setVisibility(View.VISIBLE);
+//                    Fx.slide_down(this, txt_week3);
+//                }
+//                break;
+//            case R.id.week4:
+//                if(txt_week4.isShown()){
+//                    Fx.slide_up(this, txt_week4);
+//                    txt_week4.setVisibility(View.GONE);
+//                }
+//                else{
+//                    txt_week4.setVisibility(View.VISIBLE);
+//                    Fx.slide_down(this, txt_week4);
+//                }
+//
+//
+//        }
 
 
     }
